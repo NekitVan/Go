da58dc4c2472f4bc119da11cbc74f73fbb90794f
Nik <kolonkovn@gmail.com>
2024-06-11 21:33:38 +0500

    Первая работа c api

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 )
 
-func main() {
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		log.Fatal(err)
+type info struct {
+	Marcet []struct {
+		Name  string
+		Gramm int
+		Price int
 	}
-	for _, file := range files {
-		fmt.Println(file.Name())
+}
+
+type Name struct {
+	Name string
+}
+
+func main() {
+	text := `{"marcet" : [{"name" : "Ролы Горячие", "gramm" : 1000, "price" : 1129 }, {"name" : "Запеченые", "gramm" : 1450, "price" : 2000 }, {"name" : "Холодные", "gramm" : 2000, "price" : 2300 }]}`
+	var infos info
+	json.Unmarshal([]byte(text), &infos)
+	fmt.Println(infos.Marcet[0].Name)
+
+	for i := range infos.Marcet {
+		fmt.Println(i, infos.Marcet[i].Name, infos.Marcet[i].Gramm, infos.Marcet[i].Price)
 	}
 }
