6dd508eb4f84be137da4a7e6387bbd10ab9931fb
Nik <kolonkovn@gmail.com>
2024-06-12 23:55:39 +0500

    работа c api

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 )
 
 type info struct {
-	Marcet []struct {
+	ttt []struct {
 		Name  string
-		Gramm int
+		Date  int
 		Price int
 	}
 }
 
-type Name struct {
-	Name string
+func rol() {
+	text := `{"ttt" : [{"name" :  "Бутер", "date" : 10.06.2024 , "price" : 1200} ,{"name" :  "Солянка", "date" : 13.06.2024 , "price" : 500}, {"name" :  "Пюре", "date" : 09.06.2024 , "price" : 1000}]}`
+	var infos info
+	json.Unmarshal([]byte(text), &infos)
+	fmt.Println(infos.ttt[0].Name)
+
+	for i := range infos.ttt {
+		fmt.Println(i, infos.ttt[i].Name, infos.ttt[i].Date, infos.ttt[i].Price)
+	}
 }
 
 func main() {
-	text := `{"marcet" : [{"name" : "Ролы Горячие", "gramm" : 1000, "price" : 1129 }, {"name" : "Запеченые", "gramm" : 1450, "price" : 2000 }, {"name" : "Холодные", "gramm" : 2000, "price" : 2300 }]}`
-	var infos info
-	json.Unmarshal([]byte(text), &infos)
-	fmt.Println(infos.Marcet[0].Name)
-
-	for i := range infos.Marcet {
-		fmt.Println(i, infos.Marcet[i].Name, infos.Marcet[i].Gramm, infos.Marcet[i].Price)
-	}
+	rol()
 }
