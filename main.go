eb9442501ca1e873784d42bd9319ded03e15e504
Nik <kolonkovn@gmail.com>
2024-06-17 20:34:37 +0500

    работа c api2

--- main.go
+++ main.go
@@ -5,22 +5,26 @@ import (
 	"fmt"
 )
 
-type info struct {
-	ttt []struct {
-		Name  string
-		Date  int
-		Price int
-	}
+type Info struct {
+	TTT []struct {
+		Name  string `json:"name"`
+		Date  string `json:"date"`
+		Price int    `json:"price"`
+	} `json:"ttt"`
+}
+
+type Name struct {
+	Name string
 }
 
 func rol() {
-	text := `{"ttt" : [{"name" :  "Бутер", "date" : 10.06.2024 , "price" : 1200} ,{"name" :  "Солянка", "date" : 13.06.2024 , "price" : 500}, {"name" :  "Пюре", "date" : 09.06.2024 , "price" : 1000}]}`
-	var infos info
+	text := `{"ttt" : [{"name" :  "Бутер", "date" :"10.06.2024" , "price" : 1200} ,{"name" :  "Солянка", "date" : "13.06.2024" , "price" : 500}, {"name" :  "Пюре", "date" : "09.06.2024" , "price" : 1000}]}`
+	var infos Info
 	json.Unmarshal([]byte(text), &infos)
-	fmt.Println(infos.ttt[0].Name)
+	fmt.Println(infos.TTT)
 
-	for i := range infos.ttt {
-		fmt.Println(i, infos.ttt[i].Name, infos.ttt[i].Date, infos.ttt[i].Price)
+	for i := range infos.TTT {
+		fmt.Println(i, infos.TTT[i].Name, infos.TTT[i].Date, infos.TTT[i].Price)
 	}
 }
 
